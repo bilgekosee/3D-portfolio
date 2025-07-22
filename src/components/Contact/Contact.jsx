3d4f58bf92d0447801756a0b1997c86184914ed8
bilgekosee <bilge.koosee@gmail.com>
2025-07-22 23:01:52 +0300

    email gönderme eklendi node.js kullanıldı

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,46 @@
 import "./Contact.css";
 import EarthModel from "../../Earthmodel";
 import { motion } from "framer-motion";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("form gönderiliyor");
+
+    try {
+      const res = await fetch("http://localhost:5001/send", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (res.ok) {
+        alert("Email sent successfully!");
+        setFormData({ name: "", email: "", message: "" });
+      } else {
+        alert("Failed to send email.");
+      }
+    } catch (err) {
+      console.error("Error:", err);
+      alert("Something went wrong.");
+    }
+  };
+
   return (
     <motion.section
       id="contact"
@@ -12,8 +49,9 @@ const Contact = () => {
       transition={{ duration: 0.5 }}
     >
       <div className="contact-container">
-        <motion.div
+        <motion.form
           className="info-container"
+          onSubmit={handleSubmit}
           initial={{ x: -200, opacity: 0 }}
           whileInView={{ x: 0, opacity: 1 }}
           transition={{
@@ -24,14 +62,33 @@ const Contact = () => {
           }}
           viewport={{ once: true }}
         >
-          <input type="text" placeholder="What's your name?" />
-          <input type="text" placeholder="What's your email?" />
+          <input
+            type="text"
+            name="name"
+            placeholder="What's your name?"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="What's your email?"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
           <textarea
             rows="10"
             cols="50"
+            name="message"
             placeholder="What's your message?"
+            value={formData.message}
+            onChange={handleChange}
+            required
           ></textarea>
-        </motion.div>
+          <button type="submit">Send</button>
+        </motion.form>
 
         <motion.div
           className="earth"
